0855c34bd922171f1e3f403afd0892c56602dbb8
Diego <diegoandino@gmail.com>
2023-02-06 22:09:59 -0300

    Agregado javascript, botones colapsables

---
+++ script.js
@@ -0,0 +1,21 @@
+function toggleSubSectionVisible(subsection_element_name) {
+    //document.getElementById("").innerHTML = "YOU CLICKED ME!";
+    var element = document.getElementById(subsection_element_name);
+    if(element.style.visibility == "hidden")
+    {
+        element.style.visibility = "visible";
+        element.style.maxHeight = null;
+    }
+    else
+    {
+        element.style.visibility = "hidden";
+        element.style.maxHeight = 0;
+    }
+
+  }
+
+
+
+document.getElementById("contact_button").addEventListener("click", function toggleContact() {toggleSubSectionVisible("contact_subsection")});
+document.getElementById("skills_button").addEventListener("click", function toggleSkills() {toggleSubSectionVisible("skills_subsection")});
+document.getElementById("languages_button").addEventListener("click", function toggleLanguages() {toggleSubSectionVisible("languages_subsection")});
